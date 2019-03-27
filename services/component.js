5c97ad4d0e9c40b2e0e2f586bf7c42aaeed74b2e
Markus Oberlehner <markus.oberlehner@gmail.com>
2019-03-27 06:30:55 +0100

    Clean components from migrations for API request

--- services/component.js
+++ services/component.js
@@ -1,19 +1,24 @@
 const { spaceId } = require(`../config`);
 const api = require(`../utils/api`);
 
+function removeMigrations(component) {
+  const { migrations, ...cleanComponent } = component;
+  return cleanComponent;
+}
+
 function list() {
   return api.get(`spaces/${spaceId}/components`);
 }
 
 function create({ component }) {
   return api.post(`spaces/${spaceId}/components`, {
-    component,
+    component: removeMigrations(component),
   });
 }
 
 function update({ component }) {
   return api.put(`spaces/${spaceId}/components/${component.id}`, {
-    component,
+    component: removeMigrations(component),
   });
 }
 

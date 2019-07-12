4ca8f95ac0b36156d817a45afcb4d246f3188018
Markus Oberlehner <markus.oberlehner@gmail.com>
2019-07-12 17:45:16 +0200

    Fix formatting

--- .eslintrc-md.js
+++ .eslintrc-md.js
@@ -1,10 +1,10 @@
 module.exports = {
-  "plugins": ["markdown"],
-  "extends": ".eslintrc.js",
-  "rules": {
-    "no-param-reassign": "off",
-    "quotes": [2, "single", { "avoidEscape": true }],
-    "import/no-extraneous-dependencies": false,
-    "import/no-unresolved": false
-  }
-}
+  plugins: [`markdown`],
+  extends: `.eslintrc.js`,
+  rules: {
+    'no-param-reassign': `off`,
+    quotes: [2, `single`, { avoidEscape: true }],
+    'import/no-extraneous-dependencies': false,
+    'import/no-unresolved': false,
+  },
+};

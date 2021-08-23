0d396f33050ce1e77c9d1da3d06e540273967b90
Marcus Breier <marcus.breier@starfinanz.de>
2021-08-23 12:19:58 +0200

    feat(use proxy config): storyblok-js-client config extension added

--- utils/api.js
+++ utils/api.js
@@ -2,9 +2,12 @@ const StoryblokClient = require(`storyblok-js-client`);
 
 const { oauthToken, proxy, https, httpsAgent } = require(`../config`);
 
-module.exports = new StoryblokClient({
+const sbcConfig = {
   oauthToken,
   proxy,
   https,
   httpsAgent,
-});
+};
+console.info('ECHO: sbcConfig :=', sbcConfig);
+
+module.exports = new StoryblokClient(sbcConfig);
